13573670f5573c4d6a7e0ccedb6b7be0a3db85bd
ehoversten <ehoversten@gmail.com>
2019-01-06 15:37:20 -0500

    added docstring doctest testing

--- assertions.py
+++ assertions.py
@@ -1,13 +1,40 @@
 ''' Learning testing in Python3 and TDD '''
 
-# Starting with Assertions
-def sumPosNumbers(a, b):
-    assert a >= 0 and b >= 0, "Inputs need to be positive numbers"
-    assert a != 0 and b != 0, "Inputs need to be non-zero numbers"
-    return (a + b)
+# testing Assertions
+# def sumPosNumbers(a, b):
+#     assert a >= 0 and b >= 0, "Inputs need to be positive numbers"
+#     assert a != 0 and b != 0, "Inputs need to be non-zero numbers"
+#     return (a + b)
 
 
-print(sumPosNumbers(5, 3))
-print(sumPosNumbers(0, 6))
-print(sumPosNumbers(3, -5))
+# print(sumPosNumbers(5, 3))
+# print(sumPosNumbers(0, 6))
+# print(sumPosNumbers(3, -5))
+
+# testing w/ docstrings
+def add(x, y):
+    """
+    >>> add(2, 3)
+    5
+
+    >>> add(100, 300)
+    400
+
+    """
+    return x + y
+
+def double(values):
+    """
+    >>> double([1, 2, 3, 4, 5])
+    [2, 4, 6, 8, 10]
+
+    >>> double(['a', 'b', 'c'])
+    ['aa', 'bb', 'cc']
+
+    >>> double([True, None]):
+    Traceback (most recent call last):
+        ...
+    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+    """
+    return [2 * element for element in values]
 
